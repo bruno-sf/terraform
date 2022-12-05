6dc969bfcd7f811f475d5501374936888c450daa
Bruno Ferreira <bruno.ferreira@unirio.br>
2022-12-04 21:42:38 -0300

    Change description and resources

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,9 @@
 /*
-Basically this will create a main vpc with 6 subnets(2 pub, 2 priv, 2 data) in diferrents AZ`s, with one internet gateway
+This is intended to be used as a module inside a project.
+What this module will create:
+ - a main vpc (non default);
+ - 6 subnets(2 pub, 2 priv, 2 data) in diferrents AZ's;
+ - 1 internet gateway;
 */
 resource "aws_vpc" "main_vpc" {
   cidr_block = var.vpc_cidr
@@ -9,4 +13,10 @@ resource "aws_vpc" "main_vpc" {
   tags = {
     Name = "VPC_${var.environment}_${var.region}"
   }
+}
+
+resource "aws_subnet" "subnet_pub_a" {
+    availability_zone_id = "a"
+
+  
 }
